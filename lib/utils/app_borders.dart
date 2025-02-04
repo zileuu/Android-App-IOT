87042dd99c749bb035246cf7d01af016674f9eb8
zileuu <86408505+zileuu@users.noreply.github.com>
2025-02-04 18:56:45 +0000

    Update app_borders.dart

---
+++ lib/utils/app_borders.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppBorders {
+  static const OutlineInputBorder authOutlineInputBorder = OutlineInputBorder(
+    borderSide: BorderSide(color: Color(0xFF757575)),
+    borderRadius: BorderRadius.all(Radius.circular(100)),
+  );
+}
